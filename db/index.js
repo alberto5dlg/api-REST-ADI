4fec7601cee8fdab6df9d56789a16ac3e7bba50b
Alberto De Lucas <alberto5dlg@gmail.com>
2016-10-28 16:49:08 +0200

    Cambios menores de Codigo Mensajes de conexion a la base de datos refactorizacion de Codigo

--- db/index.js
+++ db/index.js
@@ -1,5 +1,11 @@
 var mongoose = require('mongoose');
 
 exports.start = function() {
-	mongoose.connect('mongodb://localhost/adi');
+	mongoose.connect('mongodb://localhost/adi', function(err, res) {
+		if(err) {
+			console.log('ERROR: En la conexion con la Base de Datos. ' + err);
+		}
+		else 
+			console.log('Conectado a la Base de Datos');
+	});
 }
--- index.js
+++ index.js
@@ -3,20 +3,25 @@ var bodyParser = require("body-parser");
 var db = require('./db');
 var app = express();
 app.use(bodyParser.json());
+module.exports = app;
 
+//Declaracion de variables segun la ruta
 var usuario = require('./routes/usuario');
 var noticia = require('./routes/noticia');
 
+//Rutas de los metodos, segun clase
 app.use('/api/usuarios', usuario);
 app.use('/api/noticias', noticia)
 
+//Ruta generica del Server
 app.get('/',function(req, res) { 
 	res.send("Api REST para ADI");
 });
 
+//Conectamos con la Base de Datos
+db.start();
+
+//Abrimo el puerto de Escucha para el servidor Node
 app.listen(3000,function() {
 	console.log("Servidor Node ejecutandose sobre http://localhost:3000");
 })
-
-db.start();
-module.exports = app;
