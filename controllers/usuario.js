efbb985e659ab45e352d84dc16e2bb513675905f
Alberto De Lucas <alberto5dlg@gmail.com>
2016-10-28 20:41:33 +0200

    Añadidas comprobaciones y errores añadidas comprobaciones a la hora de crear y modificar usuarios, ya que si falta algun campo devuelve error 400 y no deja modificar

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -27,16 +27,24 @@ exports.findByLogin = function(pet, res) {
 //METODO POST crear un nuevo usuario 
 exports.create = function(pet, res) {
 	var usuario = new Usuario(pet.body);
-	usuario.save(function(err, newUsuario) {
-		if(err){
-			res.status(400);
-			res.send("No se puede crear el usuario, algun campo es incorrecto");
-		} else {
-			res.status(201);
-        	res.header('Location','http://localhost:3000/api/usuarios/'+ newUsuario.login);
-			res.send(newUsuario);
-		}
-	});
+
+	f(newUsuario.login == undefined || newUsuario.nombre == undefined 
+	|| newUsuario.apellidos == undefined || newUsuario.email == undefined){
+		res.status(400);
+		res.send("Faltan campos para poder crear el usuario.");
+	} 
+	else {	
+		usuario.save(function(err, newUsuario) {
+			if(err){
+				res.status(400);
+				res.send("No se puede crear el usuario, algun campo es incorrecto");
+			} else {
+				res.status(201);
+	        	res.header('Location','http://localhost:3000/api/usuarios/'+ newUsuario.login);
+				res.send(newUsuario);
+			}
+		});
+	}
 }
 
 
@@ -64,28 +72,35 @@ exports.deleteByLogin = function (pet, res) {
 exports.updateByLogin = function (pet,res) {
 	var newUsuario = new Usuario(pet.body);
 
-	Usuario.findOne({login: pet.params.login}, function(err, usuario){
-		if(usuario == undefined){
-			res.status(404);
-			res.send("El usuario a modificar no existe.");
-		} else {
-			usuario.nombre = newUsuario.nombre;
-			usuario.apellidos = newUsuario.apellidos;
-			usuario.email = newUsuario.email;
-			usuario.login = newUsuario.login;
-			Usuario.update({_id: usuario._id}, usuario, function(err) {
-				if(err) {
-					console.log(err);
-					res.status(500);
-					res.end();
-				} else {
-					res.status(204);
-					res.send(usuario);
-					res.end();
-				}
-			});
-		}
-	});
+	if(newUsuario.login == undefined || newUsuario.nombre == undefined 
+	|| newUsuario.apellidos == undefined || newUsuario.email == undefined){
+		res.status(400);
+		res.send("Faltan campos para poder editar el usuario.");
+	} 
+	else {
+		Usuario.findOne({login: pet.params.login}, function(err, usuario){
+			if(usuario == undefined){
+				res.status(404);
+				res.send("El usuario a modificar no existe.");
+			} else {
+				usuario.nombre = newUsuario.nombre;
+				usuario.apellidos = newUsuario.apellidos;
+				usuario.email = newUsuario.email;
+				usuario.login = newUsuario.login;
+				Usuario.update({_id: usuario._id}, usuario, function(err) {
+					if(err) {
+						console.log(err);
+						res.status(500);
+						res.end();
+					} else {
+						res.status(204);
+						res.send(usuario);
+						res.end();
+					}
+				});
+			}
+		});
+	}
 }
 
 
