b4886fc6f9e8cb6d540fd713d69ac0badbe9f863
Alberto De Lucas <alberto5dlg@gmail.com>
2016-10-29 13:29:33 +0200

    Metodo POST noticia

--- controllers/noticia.js
+++ controllers/noticia.js
@@ -10,13 +10,43 @@ exports.list = function(pet, res) {
 //METODO POST 
 exports.create = function(pet, res) {
 	var noticia = new Noticia(pet.body);
+	if (noticia.titular == undefined || noticia.cuerpoNoticia == undefined || noticia.autor == undefined) {
+		res.status(400);
+		res.send("Faltan campos por insertar");
+	} 
+	else {
+		noticia.fecha = fechaDeHoy();
+		Noticia.count({}, function(err,count){
+			noticia.noticiaID = count;
+			noticia.save(function(err, newNoticia) {
+				if(err){
+					res.status(500);
+					res.end();
+				} 
+				else {
+					res.status(201);
+					res.header('Location','http://localhost:3000/api/noticias/'+ noticia.noticiaID);
+					res.send(newNoticia);
+				}
+			});
+		});
+	}
+}
 
-	noticia.save(function(err, newNoticia) {
-		if(err){
-			res.json(400, err.noticia);
-		} 
-		else {
-			res.json(newNoticia);
-		}
-	});
+function fechaDeHoy(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd;
+    } 
+
+    if(mm<10) {
+        mm='0'+mm;
+    } 
+
+    today = dd+'/'+mm+'/'+yyyy;
+    return today; 
 }
--- models/noticia.js
+++ models/noticia.js
@@ -4,7 +4,9 @@ var Schema = mongoose.Schema;
 var NoticiaSchema = new Schema({
 	titular: 			{type: String},
 	cuerpoNoticia: 		{type: String},
-	autor: 				{type: String}
+	autor: 				{type: String},
+	noticiaID: 			{type: Number},
+	fecha: 				{type: String}
 });
 
 module.exports = mongoose.model('Noticia', NoticiaSchema);
