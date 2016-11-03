b56347aa07e39b656044d11d218b6578c66ae978
Alberto De Lucas <alberto5dlg@gmail.com>
2016-11-03 12:17:32 +0100

    Añadidos test de Noticias

---
+++ test/test_noticia.js
@@ -0,0 +1,61 @@
+var index = require('../index');
+var supertest = require('supertest');
+
+describe('Prueba de los metodos de noticias', function() {
+	
+	it('/noticias devuelve el contenido adecuado', function (done) {
+		supertest(index)
+			.get('/api/noticias');
+			.expect(200, done);
+	});
+
+	it('Borraremos una noticia con credenciales correctos', function(done) {
+		supertest(index)
+			.delete('/api/noticias/1', {
+				"auth": {
+					"username":"admin",
+					"password":"123456"
+				}
+			});
+			.expect(204, done);
+	});
+
+	it('Borraremos una noticia con credenciales incorrectos', function(done) {
+		supertest(index)
+			.delete('/api/noticias/2', {
+				"auth": {
+					"username":"alberto",
+					"password":"1234"
+				}
+			});
+			.expect(403, done);
+	});
+
+	it('Buscamos una noticia por ID', function(done) {
+		supertest(index)
+			.get('/api/noticias/0');
+			.expect(200, done);
+	});
+
+	it('Buscamos una noticia por ID que no existe', function(done) {
+		supertest(index)
+			.get('/api/noticias/34853');
+			.expect(404, done);
+	});
+
+	it('Crear una Noticia', function(done) {
+		supertest(index)
+			.post('/api/noticias/nuevo', {
+				"auth": {
+							"username":"alberto",
+							"password":"1234"
+						}
+				}
+			.set('Content-Type', 'application/json');
+    		.send('{"titular":"Noticias Test",
+    				"cuerpoNoticia":"Prueba de nueva noticia, Prueba de nueva noticia, Prueba de nueva noticia",
+    				"autor":"Supertest"}');
+    		.expect(201, done);
+	});
+
+});
