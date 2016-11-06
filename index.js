9798db196250c8f9e88d3e80df5a30048ce9b59a
Alberto De Lucas <alberto5dlg@gmail.com>
2016-11-06 10:56:56 +0100

    minor fix 2

--- index.js
+++ index.js
@@ -35,11 +35,6 @@ db.start();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
