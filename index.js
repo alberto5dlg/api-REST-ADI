c9d55d6412dad608454ee112967b8cdbc77266fd
Alberto De Lucas <alberto5dlg@gmail.com>
2016-11-06 10:59:27 +0100

    Direccion server

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+  console.log(__dirname);
 });
 
 
